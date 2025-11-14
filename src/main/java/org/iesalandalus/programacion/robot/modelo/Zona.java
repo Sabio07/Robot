e0d18bbed8985748a718c08147b1968937e36f62
Sabio_07 <josesabiop07@gmail.com>
2025-11-14 14:36:40 +0100

    Meddio hecho

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Zona.java
@@ -0,0 +1,12 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+public record Zona(int ancho, int alto) {
+    public static final ANCHO_MINIMO;
+    public static final ANCHO_MAXIMO;
+    public static final ALTO_MINIMO;
+    public static final ALTO_MAXIMO;
+
+    public Zona(int ancho, int alto) {
+        
+    }
+}
